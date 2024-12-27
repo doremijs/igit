3364c7962b1057e3665c333d865d30df0fd8c7a6
erguotou <erguotou5525@gmail.com>
2024-12-27 16:27:21 +0800

    feat: use zsh/bash/sh to run command

--- packages/core/src/command.rs
+++ packages/core/src/command.rs
@@ -2,14 +2,31 @@ use std::error::Error;
 use std::process::Command;
 
 pub fn run_command(command: &str, args: &str) -> Result<(), Box<dyn Error>> {
-  let output = Command::new("sh")
+  let cmd = format!("{} {}", command, args);
+
+  let shell = if Command::new("zsh").output().is_ok() {
+    "zsh"
+  } else if Command::new("bash").output().is_ok() {
+    "bash"
+  } else {
+    "sh"
+  };
+
+  let output = Command::new(shell)
+    .env("PATH", format!("{}:./node_modules/.bin", std::env::var("PATH").unwrap_or_default()))
     .arg("-c")
-    .arg(format!("PATH=$PATH:./node_modules/.bin {} {}", command, args))
+    .arg(&cmd)
     .stdout(std::process::Stdio::inherit())
     .stderr(std::process::Stdio::inherit())
     .output()?;
+
   if !output.status.success() {
-    return Err("Command failed".into());
+    let code = output.status.code().unwrap_or(-1);
+    let stderr = String::from_utf8_lossy(&output.stderr);
+    return Err(format!(
+      "Command '{}' failed with exit code {}\nError: {}",
+      cmd, code, stderr
+    ).into());
   }
   Ok(())
 }
