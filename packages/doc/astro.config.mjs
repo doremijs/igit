d9ec14779a3fd7e21bc584fa095c04f0f4116751
erguotou <erguotou525@gmail.com>
2025-01-01 11:23:53 +0800

    doc: update doc

--- packages/doc/astro.config.mjs
+++ packages/doc/astro.config.mjs
@@ -16,7 +16,7 @@ export default defineConfig({
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Guide', slug: 'guides/get-started' },
+						{ label: 'Guide', slug: 'guides/getting-started' },
 					],
 				},
 				{
