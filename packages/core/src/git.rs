9b3c243d5cfb382e355ef6c3b9ac1bf7f6ed8a9a
erguotou <erguotou5525@gmail.com>
2025-01-06 13:39:17 +0800

    ci: revert git repo test

--- packages/core/src/git.rs
+++ packages/core/src/git.rs
@@ -12,24 +12,24 @@ pub fn is_git_installed() -> bool {
 }
 
 pub fn is_git_repo() -> bool {
-  // let is_in_repo = Command::new("git")
-  //   .arg("rev-parse")
-  //   .arg("--is-inside-work-tree")
-  //   .stdout(std::process::Stdio::null())
-  //   .stderr(std::process::Stdio::null())
-  //   .status()
-  //   .map_err(|e| {
-  //     format!(
-  //       "Failed to check if current directory is a git repository: {}",
-  //       e
-  //     )
-  //   })
-  //   .unwrap()
-  //   .success();
+  let is_in_repo = Command::new("git")
+    .arg("rev-parse")
+    .arg("--is-inside-work-tree")
+    .stdout(std::process::Stdio::null())
+    .stderr(std::process::Stdio::null())
+    .status()
+    .map_err(|e| {
+      format!(
+        "Failed to check if current directory is a git repository: {}",
+        e
+      )
+    })
+    .unwrap()
+    .success();
 
-  // if !is_in_repo {
-  //   return false;
-  // }
+  if !is_in_repo {
+    return false;
+  }
   let git_dir = Path::new(".git");
   if !git_dir.exists() || !git_dir.is_dir() {
     return false;
